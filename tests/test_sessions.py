152029f2e697734f922afdf6feafacb9e70d7856
Konstantin Togoi <konstantin.togoi@gmail.com>
2020-03-20 18:36:25 +0300

    add markers

--- tests/test_sessions.py
+++ tests/test_sessions.py
@@ -12,6 +12,7 @@ from aiookru.utils import SignatureCircuit
 class TestPublicSession:
     """Tests of PublicSession class."""
 
+    @pytest.mark.asyncio
     async def test_error_request(self, error, error_server):
         async with PublicSession() as session:
             session.API_URL = error_server.url
@@ -20,6 +21,7 @@ class TestPublicSession:
             response = await session.public_request()
             assert response == error
 
+    @pytest.mark.asyncio
     async def test_error_request_with_raising(self, error_server):
         async with PublicSession() as session:
             session.API_URL = error_server.url
@@ -28,6 +30,7 @@ class TestPublicSession:
             with pytest.raises(APIError):
                 await session.public_request()
 
+    @pytest.mark.asyncio
     async def test_dummy_request(self, dummy_server, dummy):
         async with PublicSession() as session:
             session.API_URL = dummy_server.url
@@ -36,6 +39,7 @@ class TestPublicSession:
             response = await session.public_request()
             assert response == dummy
 
+    @pytest.mark.asyncio
     async def test_dummy_request_with_raising(self, dummy_server):
         async with PublicSession() as session:
             session.API_URL = dummy_server.url
@@ -44,6 +48,7 @@ class TestPublicSession:
             with pytest.raises(CustomAPIError):
                 await session.public_request()
 
+    @pytest.mark.asyncio
     async def test_data_request(self, data_server, data):
         async with PublicSession() as session:
             session.API_URL = data_server.url
@@ -68,6 +73,7 @@ class TestTokenSession:
     def token(self):
         return {'access_token': '', 'session_secret_key': ''}
 
+    @pytest.mark.asyncio
     async def test_sig_circuit(self, app, token):
         async with TokenSession(**app, **token) as session:
             assert session.sig_circuit is SignatureCircuit.UNDEFINED
@@ -79,11 +85,13 @@ class TestTokenSession:
             session.app_secret_key = 'app key'
             assert session.sig_circuit is SignatureCircuit.SERVER_SERVER
 
+    @pytest.mark.asyncio
     async def test_required_params(self, app, token):
         async with TokenSession(**app, **token) as session:
             assert 'application_key' in session.required_params
             assert 'format' in session.required_params
 
+    @pytest.mark.asyncio
     async def test_params_to_str(self, app, token):
         async with TokenSession(**app, **token) as session:
             params = {'"a"': 1, '"b"': 2, '"c"': 3}
@@ -95,6 +103,7 @@ class TestTokenSession:
             query = session.params_to_str(params)
             assert query == '"a"=1"b"=2"c"=3session key'
 
+    @pytest.mark.asyncio
     async def test_error_request(self, app, token, error_server, error):
         async with TokenSession(**app, **token) as session:
             session.API_URL = error_server.url
@@ -104,6 +113,7 @@ class TestTokenSession:
             response = await session.request(params={'key': 'value'})
             assert response == error
 
+    @pytest.mark.asyncio
     async def test_error_request_with_raising(self, app, token, error_server):
         async with TokenSession(**app, **token) as session:
             session.API_URL = error_server.url
@@ -113,6 +123,7 @@ class TestTokenSession:
             with pytest.raises(APIError):
                 _ = await session.request(params={'key': 'value'})
 
+    @pytest.mark.asyncio
     async def test_dummy_request(self, app, token, dummy_server, dummy):
         async with TokenSession(**app, **token) as session:
             session.API_URL = dummy_server.url
@@ -122,6 +133,7 @@ class TestTokenSession:
             response = await session.request(params={'key': 'value'})
             assert response == dummy
 
+    @pytest.mark.asyncio
     async def test_dummy_request_with_raising(self, app, token, dummy_server):
         async with TokenSession(**app, **token) as session:
             session.API_URL = dummy_server.url
@@ -131,6 +143,7 @@ class TestTokenSession:
             with pytest.raises(CustomAPIError):
                 await session.request(params={'key': 'value'})
 
+    @pytest.mark.asyncio
     async def test_data_request(self, app, token, data_server, data):
         async with TokenSession(**app, **token) as session:
             session.API_URL = data_server.url
@@ -156,6 +169,7 @@ class TestImplicitSession:
     def cred(self):
         return {'login': 'email@example.ru', 'passwd': 'password'}
 
+    @pytest.mark.asyncio
     async def test_get_auth_dialog(self, app, cred, httpserver, auth_dialog):
         # success
         httpserver.serve_content(**{
@@ -184,6 +198,7 @@ class TestImplicitSession:
 
         await session.close()
 
+    @pytest.mark.asyncio
     async def test_post_auth_dialog(self, app, cred, httpserver,
                                     auth_dialog, access_dialog):
         # success
@@ -204,6 +219,7 @@ class TestImplicitSession:
 
         await session.close()
 
+    @pytest.mark.asyncio
     async def test_post_access_dialog(self, app, cred, httpserver, access_dialog):
         # success
         httpserver.serve_content(**{'code': 200, 'content': 'blank page'})
@@ -223,6 +239,7 @@ class TestImplicitSession:
 
         await session.close()
 
+    @pytest.mark.asyncio
     async def test_get_access_token(self, app, cred, httpserver):
         # fail
         httpserver.serve_content(**{'code': 400, 'content': ''})
