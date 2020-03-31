9066bceec9c3afe7de63012dac892d6dba9ba060
Konstantin Togoi <konstantin.togoi@gmail.com>
2020-03-31 22:39:08 +0300

    add base parser

--- aiookru/parsers.py
+++ aiookru/parsers.py
@@ -2,51 +2,34 @@ import html.parser
 from collections import defaultdict
 
 
-class AuthDialogParser(html.parser.HTMLParser):
+class FormParser(html.parser.HTMLParser):
+    """HTML form parser."""
+
+    @property
+    def form(self):
+        return self.url, self.inputs
+
+    __slots__ = ('url', 'inputs')
+
+    def __init__(self):
+        super().__init__()
+        self.inputs = {}
+        self.url = ''
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'input':
+            attrs = defaultdict(str, attrs)
+            if attrs['type'].lower() != 'submit':
+                self.inputs[attrs['name']] = attrs['value']
+        elif tag == 'form':
+            attrs = defaultdict(str, attrs)
+            if attrs['method'].lower() == 'post':
+                self.url = attrs['action']
+
+
+class AuthDialogParser(FormParser):
     """Authorization dialog parser."""
 
-    @property
-    def form(self):
-        return self.url, self.inputs
 
-    __slots__ = ('url', 'inputs')
-
-    def __init__(self):
-        super().__init__()
-        self.inputs = {}
-        self.url = ''
-
-    def handle_starttag(self, tag, attrs):
-        if tag == 'input':
-            attrs = defaultdict(str, attrs)
-            if attrs['type'].lower() != 'submit':
-                self.inputs[attrs['name']] = attrs['value']
-        elif tag == 'form':
-            attrs = defaultdict(str, attrs)
-            if attrs['method'].lower() == 'post':
-                self.url = attrs['action']
-
-
-class AccessDialogParser(html.parser.HTMLParser):
+class AccessDialogParser(FormParser):
     """Access dialog parser."""
-
-    @property
-    def form(self):
-        return self.url, self.inputs
-
-    __slots__ = ('url', 'inputs')
-
-    def __init__(self):
-        super().__init__()
-        self.inputs = {}
-        self.url = ''
-
-    def handle_starttag(self, tag, attrs):
-        if tag == 'input':
-            attrs = defaultdict(str, attrs)
-            if attrs['type'].lower() != 'submit':
-                self.inputs[attrs['name']] = attrs['value']
-        elif tag == 'form':
-            attrs = defaultdict(str, attrs)
-            if attrs['method'].lower() == 'post':
-                self.url = attrs['action']
